0db49e66f88bc6019023013bf56cddfec9e8ef4b
ElrohirGT <elrohirgt@gmail.com>
2025-03-23 23:52:24 -0600

    feat: Added UWULexer steps

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Jose-Prince/UWULexer/lib"
+)
 
 func main() {
-	fmt.Println("Hello Nix!")
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Please ONLY supply a lex file")
+	}
+
+	lexFilePath := os.Args[1]
+	fmt.Println("Parsing file:", lexFilePath)
+
+	// TODO: Parse lex file
+	var lexFileData LexFileData
+
+	// TODO: Combine all regexes into a single regex
+	infix := []lib.RX_Token{}
+	i := 0
+	keysCount := len(lexFileData.Rule)
+	for regex, code := range lexFileData.Rule {
+		// What we want is to have something like: ((<REGEX>).(DUMMY))
+		infix = append(infix, lib.CreateOperatorToken(lib.LEFT_PAREN))
+
+		infix = append(infix, lib.CreateOperatorToken(lib.LEFT_PAREN))
+		regexToTokens := InfixToTokens(regex)
+		infix = append(infix, regexToTokens...)
+		infix = append(infix, lib.CreateOperatorToken(lib.RIGHT_PAREN))
+		infix = append(infix, lib.CreateOperatorToken(lib.AND))
+		infix = append(infix, lib.CreateDummyToken(lib.DummyInfo{Code: code}))
+
+		infix = append(infix, lib.CreateOperatorToken(lib.RIGHT_PAREN))
+
+		if i+1 < keysCount {
+			infix = append(infix, lib.CreateOperatorToken(lib.OR))
+		}
+		i++
+	}
+
+	// TODO: Regex to AFD
+
+	// TODO: Generate AFD simulator (lexer)
 }
