020c397e8c6c9c580f0946e92c17041545805d23
ElrohirGT <elrohirgt@gmail.com>
2025-05-26 09:31:26 -0600

    feat: Added parsing table

---
+++ lib/grammar/parsing.go
@@ -0,0 +1,26 @@
+package grammar
+
+import "github.com/Jose-Prince/UWULexer/lib"
+
+type AFDNodeId = string
+
+// An action can be either:
+// * a shift action
+// * a reduce action
+// * an accept state
+type Action struct {
+	// Shift to another AFDNode
+	Shift lib.Optional[AFDNodeId]
+	// Reduce according to a production in the original grammar
+	Reduce lib.Optional[int]
+	Accept bool
+}
+
+type ParsingTable struct {
+	// The Action table contains all the reduce and shifts of the parsing table.
+	ActionTable map[AFDNodeId]map[GrammarToken]Action
+	// The GoTo table contains all the nonterminal tokens and what transitions to make of them.
+	GoToTable map[AFDNodeId]map[GrammarToken]AFDNodeId
+	// The original grammar, IT MUST NOT BE EXPANDED!
+	Original Grammar
+}
